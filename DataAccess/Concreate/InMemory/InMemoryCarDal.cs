25ffb171b80fe001afeef524db3f9bd6481fe9db
TUĞÇE SEKBAN <tugceesekbann@gmail.com>
2021-03-16 15:19:34 +0300

    Add project files.

---
+++ DataAccess/Concreate/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concreate.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            { 
+            new Car{CarId=1, BrandId=1, ColorId=1, DailyPrice=300, Description="Model 2010", ModelYear=2010},
+            new Car{CarId=2, BrandId=1, ColorId=3, DailyPrice=400, Description="Model 2012", ModelYear=2012},
+            new Car{CarId=3, BrandId=2, ColorId=1, DailyPrice=500, Description="Model 2014", ModelYear=2014}
+            
+            };
+
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(p => p.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetByIdCar(int carId)
+        {
+            return _cars.Where(p => p.CarId == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+
+        }
+    }
+}
---
+++ Entities/Concreate/Color.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concreate
+{
+   public class Color:IEntity
+    {
+        public int ColorId { get; set; }
+        public int ColorName { get; set; }
+    }
+}
