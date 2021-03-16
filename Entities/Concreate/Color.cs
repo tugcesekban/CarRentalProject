5d97ad7aa309bf8e00860706cda6649e6ff6f6a5
TUĞÇE SEKBAN <tugceesekbann@gmail.com>
2021-03-16 15:31:57 +0300

    CarRentalProject

--- Entities/Concreate/Color.cs
+++ Entities/Concreate/Color.cs
@@ -8,6 +8,6 @@ namespace Entities.Concreate
    public class Color:IEntity
     {
         public int ColorId { get; set; }
-        public int ColorName { get; set; }
+        public string ColorName { get; set; }
     }
 }
